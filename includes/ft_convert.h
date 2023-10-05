15bb8f93c200f1b6f57301b39b3352f370c2bc3f
Bruno Miguel <brunomiguel0798@gmail.com>
2023-10-05 22:37:28 +0100

    Comentarios na biblioteca ft_convert.h

--- includes/ft_convert.h
+++ includes/ft_convert.h
@@ -9,8 +9,35 @@
 #include <ctype.h>
 
 int ft_atoi(const char *str);
+/*
+ * @brief Converts a string to an integer
+ *
+ * @param nptr The pointer of the string to convert
+ * @return int The integer converted from the string
+ */
+
 char *ft_itoa(int n);
+/*
+ * @brief Converts an integer to a string
+ *
+ * @param n The integer to convert
+ * @return char* The converted integer
+ */
+
 int ft_tolower(int c);
+/*
+ * @brief Converts an uppercase character to a lowercase character
+ *
+ * @param c The character to convert
+ * @return int The converted character
+ */
+
 int ft_toupper(int c);
+/*
+ * @brief Converts a lowecase character to an uppercase character
+ *
+ * @param c The character to convert
+ * @return int The converted character
+ */
 
 #endif
