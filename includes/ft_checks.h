eddace49211a57061a044bc5cb87f0cf8fde7e76
Bruno0798 <brunomiguel0798@gmail.com>
2023-12-17 04:43:00 +0000

    changed a doc from a function

--- includes/ft_checks.h
+++ includes/ft_checks.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_checks.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bsousa-d <bsousa-d@student.42.fr>          +#+  +:+       +#+        */
+/*   By: bsousa-d <bsousa-d@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/07 12:12:16 by bsousa-d          #+#    #+#             */
-/*   Updated: 2023/10/07 12:13:19 by bsousa-d         ###   ########.fr       */
+/*   Updated: 2023/12/17 04:42:43 by bsousa-d         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,13 +20,13 @@
 # include <unistd.h>
 # include <ctype.h>
 
-int	ft_isalnum(int c);
-/*
+/**
  * @brief Checks if a character is alphanumeric
  *
  * @param c The character to check
  * @return int 1 if the character is alphanumeric, 0 otherwise
  */
+int	ft_isalnum(int c);
 
 int	ft_isalpha(int c);
 /*
